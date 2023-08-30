ac12a0d49fc060ad9f2a98ae9b57b91ea461b8b6
Benjamin Wagner <bnm.wag@gmail.com>
2023-08-30 16:38:04 +0200

    fix type errors

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -4,7 +4,7 @@ import { redirect } from 'next/navigation';
 import NavigationAction from './navigation-action';
 import { Separator } from '@/components/ui/separator';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import NavigationItem from './navigation-item';
+import { NavigationItem } from './navigation-item';
 import { ModeToggle } from '../mode-toggle';
 import { UserButton } from '@clerk/nextjs';
 
